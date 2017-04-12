c4b91d156b01ce498a89e9d602567087f05c2d94
kyangc <kyangc@gmail.com>
2017-04-12 16:02:15 +0800

    * add: add script and style file

---
+++ install_os.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Installs OSAndroid Code style into your AndroidStudio/IntelliJ
+
+echo "Installing Oversea Android Code Style ..."
+
+# Config file path
+URL=""
+
+#Config file name
+FILE="OsAndroid.xml"
+
+# Download config
+curl --silent -o $FILE $URL
+
+for i in $HOME/Library/Preferences/IntelliJIdea*  \
+         $HOME/Library/Preferences/IdeaIC*        \
+         $HOME/Library/Preferences/AndroidStudio* \
+         $HOME/.IntelliJIdea*/config              \
+         $HOME/.IdeaIC*/config                    \
+         $HOME/.AndroidStudio*/config
+do
+  if [[ -d $i ]]; then
+
+    # Copy file into codestyles
+    mkdir -p $i/codestyles
+    cp -frv ./$FILE $i/codestyles
+
+  fi
+done
+
+# Remove source file
+rm ./$FILE
+
+echo "Done. Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'OsAndroid'."
